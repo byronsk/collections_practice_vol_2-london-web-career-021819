7cb42b4dd2afc6abbe63f835ded79af185275d41
vyskoutaris <vyskoutaris@hotmail.com>
2019-02-07 23:37:44 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,5 +1,28 @@
 # your code goes here
 
 def begins_with_r(tools)
+end
+
+def contain_a
+end
+
+def first_wa
+end
+
+def remove_non_string(array)
+end
+
+def remove_non_string(array)
+end
+
+def count_elements(array)
+end
+
+def merge_data
+end
+
+def find_cool
+end
   
-  
+def organize_school
+end
