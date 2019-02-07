3aba9c39eb8f4e8386d4b8679cf486319ab827e2
vyskoutaris <vyskoutaris@hotmail.com>
2019-02-07 23:58:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,8 +1,8 @@
 # your code goes here
 
-  def begins_with_r(tools)
-    tools.all? do | words |
-   words.start_with? "r"
+def begins_with_r(tools)
+  tools.all? do | words |
+  words.start_with? "r"
   end
 end
 
